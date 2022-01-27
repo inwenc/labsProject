ed42c75c43671d8072db826745fd48caa771d67a
yingwen chen <yingwen.chen@balena.io>
2022-01-26 19:08:01 -0600

    checkpoints saves in db

--- project-api/server/index.js
+++ project-api/server/index.js
@@ -1,38 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const db = require('../db/conn');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const { db, Checkpoints } = require("../db/conn");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from server!' });
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
-app.post('/postCheckpoints', (req, res) => {
-  let dataSet = req.body
-  console.log('dataSet', dataSet)
-  // insertDataSet(dataSet, (err, results)=>{
-  //   if (err) {
-  //     console.log('err', err);
-  //   } else {
-  //     var stringifyResults = JSON.stringify(results);
-  //     res.end(stringifyResults);
-  //   }
-  // })
-  res.json('received')
+app.post("/postCheckpoints", async (req, res) => {
+  let dataSet = req.body;
 
-})
+  const checkpoints = new Checkpoints({
+    checkpoints: dataSet,
+  });
+  await checkpoints.save(function (err) {
+    if (err) return handleError(err);
+    console.log("saved!");
+  });
 
+  res.json("received");
+});
 
-  mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
